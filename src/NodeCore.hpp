c9c8b4721dfb58661bdbfd8294cfe913eabaa4c3
Damian Drzewicz <damian.drzewicz@gmail.com>
2022-02-05 23:23:56 +0100

    Closes #15, include radio library

--- src/NodeCore.hpp
+++ src/NodeCore.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
+#include <Arduino.h>
+#include <SPI.h>
+#include "RFM69_ATC.h"
+
+#define FLASH_SS 8
+
 class NodeCore{
 public:
     void test();
+
+private:
+    RFM69_ATC radio_;
 };
