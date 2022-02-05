0f712034acab4ed4cc2a0dcfd22a79a966312ba8
Damian Drzewicz <damian.drzewicz@gmail.com>
2022-02-06 00:43:38 +0100

    Issue #16, add missing implementations

--- src/RadioConfig.hpp
+++ src/RadioConfig.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-
 #include "EEPROMConfig.hpp"
+#include <EEPROM.h>
 
 class RadioConfig : public EEPROMConfig
 {
@@ -19,7 +19,29 @@ public:
 
     RadioConfig(int address) : EEPROMConfig(address) {}
 
-    virtual void save() override {};
-    virtual void read() override {};
-    virtual void clear() override {};
+    virtual void save() override {
+        EEPROM.put(address_, data_);
+    };
+
+    virtual void read() override {
+        EEPROM.get(address_, data_);
+        if(data_.isEmpty()) {
+            // EEPROM was empty -> initialize default
+            data_.gatewayId = 1;
+            data_.networkId = 100;
+            data_.customFrequency = 0;
+            data_.powerLevel = 0;
+            strcpy_P(data_.encryptKey, PSTR("sampleEncryptKey"));
+        }
+    };
+
+    virtual void clear() override {
+        for (unsigned int i = address_; i < address_ + sizeof(data_); i++)
+        {
+            EEPROM.write(i, 0xff);
+        }
+    };
+
+private:
+    RadioConfigData data_;
 };
